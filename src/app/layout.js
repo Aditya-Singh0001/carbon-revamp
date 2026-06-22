5d5fdfe67174480a95c6713cb5a5ddeaaf02fd85
Aditya-Singh0001 <123adityasingh10@gmail.com>
2026-06-23 03:45:35 +0530

    migrated the About Page in NEXT

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar";
 import ScrollManager from "@/components/scrollmanager";
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body>
         <ScrollManager />
         <Navbar />
